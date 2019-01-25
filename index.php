a5b0e3b39a262d39e794c999f6052b5570f8fbb7
Jason Wuerch <wuerchjason@gmail.com>
2019-01-24 22:48:40 -0300

    more site updates

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="sign-up-box">
+			<section class="sign-up-box">
 				<div class="wrap">
 					<h2 class="main-text">Want to learn about even more ways to earn extra&nbsp;money?</h2>
 					<div class="small-text">Join our mailing list to get access to exclusive articles, courses and money&nbsp;making&nbsp;tips.</div>
@@ -35,54 +35,123 @@
 					</form>
 				</div>
 
-			</div>
+			</section>
 			<div class="feature-box">
 				<div class="wrap">
 
 				</div>
 			</div>
+			<section class="most-popular">
+				<div class="wrap">
+					<h2 class="section-title">Featured Articles</h2>
+					<div class="row">
+						<div class="col-1 col">
+							<div class="art-1 art">
+								<div class="img-wrap">
+									<img src="https://mk0frugalforlesh94so.kinstacdn.com/wp-content/uploads/2018/12/what-to-do-if-your-identity-has-been-stolen-247x187.jpg">
+								</div>
+								<div class="transcript">
+									<div class="category">
+										<h6>Make Money</h6>
+									</div>
+									<div class="title">
+										<h2>25+ Best Survey Survey Sites: Earn $600+/Month</h2>
+									</div>
+									<div class="excerpt">
+										<p>With thousands of survey sites claiming they can make you thousands, it's difficult to find which ones are scam or legit.</p>
+									</div>
+									<div class="read-more">
+										<p>Read More <span class="arrow">→</span></p>
+									</div>
+								</div>
 
-			<div id="content">
+							</div>
+						</div>
+						<div class="col-2 col">
+							<div class="art-2 art">
+								<div class="img-wrap">
+									<img src="https://mk0frugalforlesh94so.kinstacdn.com/wp-content/uploads/2018/12/lifestyle-changes-to-save-money-247x187.jpg">
+								</div>
+								<div class="transcript">
+									<div class="category">
+										<h6>Make Money</h6>
+									</div>
+									<div class="title">
+										<h2>101 Easy &amp; Practical Ways To Make Money Now</h2>
+									</div>
+									<div class="excerpt">
+										<p>Looking for cash and need it fast? The methods below are ways in which you can make money now, or rather quickly.</p>
+									</div>
+									<div class="read-more">
+										<p>Read More <span class="arrow">→</span></p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-3 col">
+							<div class="art-3 art">
+								<div class="img-wrap">
+									<img src="https://mk0frugalforlesh94so.kinstacdn.com/wp-content/uploads/2018/12/earnably-review-247x187.jpg">
+								</div>
+								<div class="transcript">
+									<div class="category">
+										<h6>Make Money</h6>
+									</div>
+									<div class="title">
+										<h2>101 Free Money Making Apps For Your Phone</h2>
+									</div>
+									<div class="excerpt">
+										<p>Want to earn free cash from while on the go and quickly? This list of 101 money making apps allows you to do just that.</p>
+									</div>
+									<div class="read-more">
+										<p>Read More <span class="arrow">→</span></p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="content">
 
 				<div id="inner-content" class="wrap cf">
 
 
 					<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+						<h2 class="section-title">Recent Posts</h2>
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+								<?php
+
+									$thumb_id = get_post_thumbnail_id();
+									$thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true);
+									?><div class="img-wrap"><img src="<?php echo $thumb_url[0]; ?>"></div>
+
+
+
+								<div class="transcript">
 
 								<header class="article-header">
 
-									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+									<h2 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 									<p class="byline entry-meta vcard">
-                                                                        <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
+                                                                        <?php printf( __( '', 'bonestheme' ).' %1$s %2$s',
                        								/* the time the post was published */
                        								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
                        								/* the author of the post */
-                       								'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    							); ?>
+                       								'<span class="by">'.__( 'in', 'bonestheme').'</span>'
+                    							); ?><?php printf( '<span class="footer-category">' . __('', 'bonestheme' ) . ' %1$s</span>' , get_the_category_list(', ') ); ?>
+										<div class="read-more"><p><a href="<?php the_permalink() ?>">Read More <span class="arrow">→</span></a></p></div>
+
+
+
 									</p>
 
-								</header>
+								</header></div>
 
-								<section class="entry-content cf">
-									<?php the_content(); ?>
-								</section>
-
-								<footer class="article-footer cf">
-									<p class="footer-comment-count">
-										<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );?>
-									</p>
-
-
-                 	<?php printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
-
-                  <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-
-								</footer>
 
 							</article>
 
@@ -109,11 +178,10 @@
 
 						</main>
 
-					<?php get_sidebar(); ?>
 
 				</div>
 
-			</div>
+			</section>
 
 
 <?php get_footer(); ?>
