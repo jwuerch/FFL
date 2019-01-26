69cba7884af7fc3ddbbfa2e180131b51003807da
Jason Wuerch <wuerchjason@gmail.com>
2019-01-26 15:03:46 -0300

    recent posts

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
             <section class="email-sign-up-section">
                 <div class="email-sign-up">
                     <div class="wrap">
-                        <h2 class="main-text">Want to learn about even more ways to earn extra&nbsp;money?</h2>
+                        <h2 class="main-text">Want to learn about even more ways to earn&nbsp;extra&nbsp;money?</h2>
                         <div class="small-text">Join our mailing list to get access to exclusive articles, courses and money&nbsp;making&nbsp;tips.</div>
                         <form class="email-form">
                             <input type="text" placeholder="First Name">
@@ -46,78 +46,75 @@
                 </div>
             </section>
 
-    <div class="feature-box">
+			<section class="popular-posts-section">
 				<div class="wrap">
+					<h2 class="section-title">Most Popular Posts</h2>
+                    <hr class="section-title-spacer">
 
-				</div>
-			</div>
-			<section class="most-popular">
-				<div class="wrap">
-					<h2 class="section-title">Popular Posts</h2>
-					<div class="row">
+                    <div class="row">
 						<div class="col-1 col">
-							<div class="art-1 art">
+							<div class="grid-item">
 								<div class="img-wrap">
 									<img src="https://mk0frugalforlesh94so.kinstacdn.com/wp-content/uploads/2018/12/what-to-do-if-your-identity-has-been-stolen-247x187.jpg">
 								</div>
-								<div class="transcript">
+								<div class="text-wrap">
 									<div class="category">
-										<h6>Make Money</h6>
+										<p>Make Money</p>
 									</div>
 									<div class="title">
 										<h2>25+ Best Survey Survey Sites: Earn $600+/Month</h2>
 									</div>
-									<div class="excerpt">
-										<p>With thousands of survey sites claiming they can make you thousands, it's difficult to find which ones are scam or legit.</p>
-									</div>
-									<div class="read-more">
-										<p>Read More <span class="arrow">→</span></p>
-									</div>
+                                    <div class="excerpt">
+                                        <p>Want to earn free cash from while on the go and quickly? This list of 101 money making apps allows you to do just that.</p>
+                                    </div>
+									<p class="read-more">
+										Read More <span class="arrow">→</span>
+									</p>
 								</div>
-
 							</div>
 						</div>
 						<div class="col-2 col">
-							<div class="art-2 art">
-								<div class="img-wrap">
-									<img src="https://mk0frugalforlesh94so.kinstacdn.com/wp-content/uploads/2018/12/lifestyle-changes-to-save-money-247x187.jpg">
-								</div>
-								<div class="transcript">
-									<div class="category">
-										<h6>Make Money</h6>
-									</div>
-									<div class="title">
-										<h2>101 Easy &amp; Practical Ways To Make Money Now</h2>
-									</div>
-									<div class="excerpt">
-										<p>Looking for cash and need it fast? The methods below are ways in which you can make money now, or rather quickly.</p>
-									</div>
-									<div class="read-more">
-										<p>Read More <span class="arrow">→</span></p>
-									</div>
-								</div>
-							</div>
+                            <div class="grid-item">
+                                <div class="img-wrap">
+                                    <img src="https://mk0frugalforlesh94so.kinstacdn.com/wp-content/uploads/2018/12/lifestyle-changes-to-save-money-247x187.jpg">
+                                </div>
+                                <div class="text-wrap">
+                                    <div class="category">
+                                        <p>Make Money</p>
+                                    </div>
+                                    <div class="title">
+                                        <h2>101 Easy &amp; Practical Ways To Make Money Now</h2>
+                                    </div>
+                                    <div class="excerpt">
+                                        <p>Want to earn free cash from while on the go and quickly? This list of 101 money making apps allows you to do just that.</p>
+                                    </div>
+                                    <p class="read-more">
+                                        Read More <span class="arrow">→</span>
+                                    </p>
+                                </div>
+                            </div>
 						</div>
 						<div class="col-3 col">
-							<div class="art-3 art">
-								<div class="img-wrap">
-									<img src="https://mk0frugalforlesh94so.kinstacdn.com/wp-content/uploads/2018/12/earnably-review-247x187.jpg">
-								</div>
-								<div class="transcript">
-									<div class="category">
-										<h6>Make Money</h6>
-									</div>
-									<div class="title">
-										<h2>101 Free Money Making Apps For Your Phone</h2>
-									</div>
-									<div class="excerpt">
-										<p>Want to earn free cash from while on the go and quickly? This list of 101 money making apps allows you to do just that.</p>
-									</div>
-									<div class="read-more">
-										<p>Read More <span class="arrow">→</span></p>
-									</div>
-								</div>
-							</div>
+                            <div class="grid-item">
+                                <div class="img-wrap">
+                                    <img src="https://mk0frugalforlesh94so.kinstacdn.com/wp-content/uploads/2018/12/earnably-review-247x187.jpg">
+                                </div>
+                                <div class="text-wrap">
+                                    <div class="category">
+                                        <p>Make Money</p>
+                                    </div>
+                                    <div class="title">
+                                        <h2>101 Free Money Making Apps For Your Phone</h2>
+
+                                    </div>
+                                    <div class="excerpt">
+                                        <p>Want to earn free cash from while on the go and quickly? This list of 101 money making apps allows you to do just that.</p>
+                                    </div>
+                                    <p class="read-more">
+                                        Read More <span class="arrow">→</span>
+                                    </p>
+                                </div>
+                            </div>
 						</div>
 					</div>
 				</div>
@@ -128,10 +125,12 @@
 				<div id="inner-content" class="wrap cf">
 
 
-					<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-						<h2 class="section-title">Recent Posts</h2>
+					<main id="main wrap" class="cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+						<h2 class="section-title">Recent Articles</h2>
+                        <hr class="section-title-spacer">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
 								<?php
@@ -142,19 +141,19 @@
 
 
 
-								<div class="transcript">
+
 
 								<header class="article-header">
 
+                                    <div class="category"><?php printf( '<div class="entry-category category mobile-category">' . __('', 'bonestheme' ) . ' %1$s</div>' , get_the_category_list(', ') ); ?></div>
 									<h2 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-									<p class="byline entry-meta vcard">
+									<div><span class="byline entry-meta vcard">
                                                                         <?php printf( __( '', 'bonestheme' ).' %1$s %2$s',
                        								/* the time the post was published */
                        								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
                        								/* the author of the post */
                        								'<span class="by">'.__( 'in', 'bonestheme').'</span>'
-                    							); ?><?php printf( '<span class="footer-category">' . __('', 'bonestheme' ) . ' %1$s</span>' , get_the_category_list(', ') ); ?></p>
-										<div class="excerpt"><?php the_excerpt(); ?></div>
+                    							); ?></span><?php printf( '<span class="entry-category category desktop-category">' . __('', 'bonestheme' ) . ' %1$s</span>' , get_the_category_list(', ') ); ?></div>
 
                                     <div class="read-more"><p><a href="<?php the_permalink() ?>">Read More <span class="arrow">→</span></a></p></div>
 
@@ -162,7 +161,7 @@
 
 									</p>
 
-								</header></div>
+								</header>
 
 
 							</article>
